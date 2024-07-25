b5687aec08aac8222dae26caa4d4a12fee49042b
ZapApriendo <isaiah.capers@outlook.com>
2024-07-24 22:38:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import { type } from 'os';
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type:'input',
+        name:'name',
+        message:'message',
+    },
+    {
+        type:'input',
+        name:'name',
+        message:'message',
+    },
+    {
+        type:'input',
+        name:'name',
+        message:'message',
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
